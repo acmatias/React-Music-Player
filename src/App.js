dbb4aaa37770c879f997b12e590dc6f9ebbb9aa9
Adrian <48665902+acmatias@users.noreply.github.com>
2022-03-15 21:41:03 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import './styles/App.scss'
+import Player from './components/Player.js'
+import Song from './components/Song.js'
+import Library from './components/Library.js'
+import Nav from './components/Nav.js'
+// data
+import data from './data.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // ref
+    const audioRef = useRef(null)
+    // State
+    const [songs, setSongs] = useState(data())
+    const [currentSong, setCurrentSong] = useState(songs[0])
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    const [libraryStatus, setLibraryStatus] = useState(false)
+
+    return (
+        <div className={`App ${libraryStatus ? 'library-active' : ''}`}>
+            <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+            <Song currentSong={currentSong} />
+            <Player
+                audioRef={audioRef}
+                setIsPlaying={setIsPlaying}
+                isPlaying={isPlaying}
+                currentSong={currentSong}
+                setCurrentSong={setCurrentSong}
+                songs={songs}
+            />
+            <Library
+                songs={songs}
+                setSongs={setSongs}
+                currentSong={currentSong}
+                setCurrentSong={setCurrentSong}
+                isPlaying={isPlaying}
+                libraryStatus={libraryStatus}
+            />
+        </div>
+    )
 }
 
-export default App;
+export default App
